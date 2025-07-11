325a7e2a55bcc41f7aaa0f5607bcb7709a002b2d
mojojompe <157244574+mojojompe@users.noreply.github.com>
2025-07-11 20:27:56 +0100

    ..2

--- emmanuel_jompe_portfolio/src/assets/Splash/Splash.tsx
+++ emmanuel_jompe_portfolio/src/assets/Splash/Splash.tsx
@@ -1,26 +1,11 @@
-import { useState, useEffect } from "react";
 import "./Splash.css";
 
 
 const Splash = () => {
-  const text = "Welcome to My Portfolio!!";
-  const [displayed, setDisplayed] = useState("");
-
-  useEffect(() => {
-    let i = 0;
-    const interval = setInterval(() => {
-      setDisplayed(text.slice(0, i + 1));
-      i++;
-      if (i >= text.length) clearInterval(interval);
-    }, 80);
-    return () => clearInterval(interval);
-  }, [text]);
-
   return (
     <div className="splash-screen">
       <img src="/LogoBlack.png" alt="splash-logo" />
       <h1>Loading...</h1>
-      <h1>{displayed}</h1>
     </div>
   );
 };
