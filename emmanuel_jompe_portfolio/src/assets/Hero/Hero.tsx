e8036d75f857266ce31c6fa4c44a61c584760a3b
mojojompe <157244574+mojojompe@users.noreply.github.com>
2025-07-11 20:26:09 +0100

    ..1

--- emmanuel_jompe_portfolio/src/assets/Hero/Hero.tsx
+++ emmanuel_jompe_portfolio/src/assets/Hero/Hero.tsx
@@ -1,4 +1,6 @@
-import './Hero.css'
+import "./Hero.css";
+import logo from "../assets/Navbar/LogoBlack.png";
+
 const Hero = () => {
   return (
     <>
@@ -9,20 +11,19 @@ const Hero = () => {
           data-aos-duration="1500"
         >
           <h1 className="hero-title">
-            Hi, I'm{" "}
-            <span>
-              Emmanuel
-            </span>
+            Hi, I'm <span>Emmanuel</span>
           </h1>
           <p className="hero-subtitle">
             I am a Software Engineer with a extensive experience with
-            development tools and a passion for creating innovative solutions, designing scalable systems and dedicated to
-            contributing immensely to the Tech World
+            development tools and a passion for creating innovative solutions,
+            designing scalable systems and dedicated to contributing immensely
+            to the Tech World
           </p>
+          <img src={logo} alt="logo" />
         </div>
       </section>
     </>
   );
-}
+};
 
 export default Hero;
