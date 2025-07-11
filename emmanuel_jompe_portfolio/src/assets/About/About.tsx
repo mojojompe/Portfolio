495981e6204e2cee8406dcd1a3a2a76e92530392
mojojompe <157244574+mojojompe@users.noreply.github.com>
2025-07-11 14:57:06 +0100

    Tech Stack Update

--- emmanuel_jompe_portfolio/src/assets/About/About.tsx
+++ emmanuel_jompe_portfolio/src/assets/About/About.tsx
@@ -1,7 +1,8 @@
 import './About.css'
 import { FaChalkboardTeacher } from 'react-icons/fa'
-import { FaNetworkWired, FaTerminal } from 'react-icons/fa6'
+import { FaJava, FaLeaf, FaNetworkWired, FaTerminal } from 'react-icons/fa6'
 import { MdDevices } from 'react-icons/md'
+import { SiAmazonwebservices, SiCplusplus, SiCss3, SiDjango, SiDocker, SiExpo, SiExpress, SiFirebase, SiGnubash, SiGooglecloud, SiGooglecloudstorage, SiHtml5, SiJavascript, SiMongodb, SiNativescript, SiNodedotjs, SiPython, SiR, SiReact, SiRedis, SiRedux, SiTypescript } from 'react-icons/si'
 const About = () => {
   return (
     <section className="me-about">
@@ -16,7 +17,8 @@ const About = () => {
               <FaTerminal size={40} color="#058789" />
               <h3>Software Engineering</h3>
               <p>
-                Provides modern and affordable creation and management of software systems and best practices 
+                Provides modern and affordable creation and management of
+                software systems and best practices
               </p>
             </div>
           </div>
@@ -28,7 +30,8 @@ const About = () => {
               <MdDevices size={40} color="#058789" />
               <h3>Cross-Platform Development</h3>
               <p>
-                Provides Web, Mobile and Desktop Software Platforms. Get Mobile Apps, Websites and Portals  
+                Provides Web, Mobile and Desktop Software Platforms. Get Mobile
+                Apps, Websites and Portals
               </p>
             </div>
           </div>
@@ -40,7 +43,8 @@ const About = () => {
               <FaNetworkWired size={40} color="#058789" />
               <h3>Networking and CyberSecurity</h3>
               <p>
-                Creates, Monitors and Manages Networks, Detects and Corrects Vulnerability and Cyber Threats 
+                Creates, Monitors and Manages Networks, Detects and Corrects
+                Vulnerability and Cyber Threats
               </p>
             </div>
           </div>
@@ -52,8 +56,8 @@ const About = () => {
               <FaChalkboardTeacher color="#058789" size={40} />
               <h3>Tutoring</h3>
               <p>
-                Provides personalized help with coding, software development, and
-                other tech-related subjects.
+                Provides personalized help with coding, software development,
+                and other tech-related subjects.
               </p>
             </div>
           </div>
@@ -61,8 +65,63 @@ const About = () => {
       </div>
 
       <div className="about-head">
-        <h2 className="about-us-h2">Frameworks</h2>
-        
+        <h2 className="about-us-h2">My Tech Stack</h2>
+      </div>
+
+      <div className="container-stack">
+        <div className="container-stack-items">
+          <h3 className="about-us-h3">Front-end</h3>
+          <div className="frameworks-grid">
+            <SiHtml5 size={50} color="#058789" />
+            <SiCss3 size={50} color="#058789" />
+            <SiJavascript size={50} color="#058789" />
+            <SiTypescript size={50} color="#058789" />
+          </div>
+        </div>
+
+        <div className="container-stack-items">
+          <h3 className="about-us-h3">Back-end</h3>
+          <div className="frameworks-grid">
+            <SiDjango size={50} color="#058789" />
+            <FaLeaf size={50} color="#058789" />
+            <SiNodedotjs size={50} color="#058789" />
+            <SiExpress size={50} color="#058789" />
+            <SiMongodb size={50} color="#058789" />
+            <SiRedis size={50} color="#058789" />
+            <SiFirebase size={50} color="#058789" />
+          </div>
+        </div>
+
+        <div className="container-stack-items">
+          <h3 className="about-us-h3">Languages</h3>
+          <div className="frameworks-grid">
+            <FaJava size={50} color="#058789" />
+            <SiPython size={50} color="#058789" />
+            <SiCplusplus size={50} color="#058789" />
+            <SiR size={50} color="#058789" />
+          </div>
+        </div>
+
+        <div className="container-stack-items">
+          <h3 className="about-us-h3">Frameworks</h3>
+          <div className="frameworks-grid">
+            <SiExpo size={50} color="#058789" />
+            <SiNativescript size={50} color="#058789" />
+            <SiReact size={50} color="#058789" />
+            <SiRedux size={50} color="#058789" />
+          </div>
+        </div>
+
+        <div className="container-stack-items">
+          <h3 className="about-us-h3">DevOps</h3>
+          <div className="frameworks-grid">
+            <SiAmazonwebservices size={50} color="#058789" />
+            <SiDocker size={50} color="#058789" />
+            <SiGooglecloud size={50} color="#058789" />
+            <SiGnubash size={50} color="#058789" />
+          </div>
+        </div>
+
       </div>
     </section>
   );
